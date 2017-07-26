fbf0ab1257cff76c7c31bbe35219d6e129b3c444
rayGreycloud <rayGreycloud@gmail.com>
2017-07-26 16:55:03 -0700

    Add passport with google oauth strategy

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT, function() {
   console.log(`Express server has started on port ${PORT}...`);
