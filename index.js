4085ecf362a7568ba3322605f4a290426463d8ba
rayGreycloud <rayGreycloud@gmail.com>
2017-07-26 13:39:35 -0700

    Refactor server port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
 app.listen(PORT, function() {
-  console.log('Express server has started on port ' + PORT + '...');
+  console.log(`Express server has started on port ${PORT}...`);
 });
