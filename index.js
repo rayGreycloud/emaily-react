d7aeb5069417935c65f82cbc1fb497479f51ef2e
rayGreycloud <rayGreycloud@gmail.com>
2017-07-27 08:44:07 -0700

    Minor refactor of authRoutes require

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 // Include passport file
-require("./services/passport.js");
-// Include auth routes
-const authRoutes = require('./routes/authRoutes');
+require('./services/passport.js');
 
 const app = express();
 
-authRoutes(app);
+// Call authRoutes with app
+// Skip variable assignment - DRY
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
