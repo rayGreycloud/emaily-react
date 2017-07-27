e5163f0fa4df604b0bbbea3472fc17971b79672b
rayGreycloud <rayGreycloud@gmail.com>
2017-07-27 14:41:11 -0700

    Add user model and comments

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+// Include dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+// Include keys
 const keys = require("./config/keys");
 // Include passport file
 require("./services/passport.js");
-
+// Include user model
+require("./models/User");
+// Connect to mLab instance
 mongoose.connect(keys.mongoURI);
-
+// Instantiate app
 const app = express();
 
 // Call authRoutes with app
 // Skip variable assignment - DRY
 require("./routes/authRoutes")(app);
 
+// Set port for heroku or local
 const PORT = process.env.PORT || 5000;
+// Start server
 app.listen(PORT, function() {
   console.log(`Express server has started on port ${PORT}...`);
 });
