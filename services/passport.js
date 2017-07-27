1bf907c926d26ccde3ff0e8cb2b737b037e672df
rayGreycloud <rayGreycloud@gmail.com>
2017-07-27 08:33:44 -0700

    Refactor passport logic

---
+++ services/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+// Set up GoogleStrategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Testing - confirms response
+      console.log("access token: ", accessToken);
+      console.log("refresh token: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
