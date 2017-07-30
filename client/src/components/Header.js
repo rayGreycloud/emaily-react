dfaeecea2fad7a00b213b7c1e0394184129bec4a
rayGreycloud <rayGreycloud@gmail.com>
2017-07-30 08:22:32 -0700

    Add styling to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="brand-logo left">Emaily</a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
