9366f8f18d43416722acb654c0aa326e6a8fe127
rayGreycloud <rayGreycloud@gmail.com>
2017-07-30 10:04:40 -0700

    Add redux-thunk to action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,13 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {};
+const fetchUser = () => {
+  return function(dispatch) {
+    axios.get("/api/current_user").then(res =>
+      dispatch({
+        type: FETCH_USER,
+        payload: res
+      })
+    );
+  };
+};
