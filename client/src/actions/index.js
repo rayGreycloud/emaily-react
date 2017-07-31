d98269e0aadd0a9fd1cdffd3c926eca05e728daa
rayGreycloud <rayGreycloud@gmail.com>
2017-07-31 08:01:38 -0700

    Update authReducer to return null, false or user object

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,8 +4,8 @@ import { FETCH_USER } from "./types";
 // Refactor using arrow and async/await
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
-
-  dispatch({ type: FETCH_USER, payload: res });
+  // Only need data prop
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 // export const fetchUser = () => {
