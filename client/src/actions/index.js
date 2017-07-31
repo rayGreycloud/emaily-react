2a8e7b97afcec685a667b5c99e9f6ed25940d766
rayGreycloud <rayGreycloud@gmail.com>
2017-07-31 06:38:04 -0700

    Refactor using arrow and async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get("/api/current_user").then(res =>
-      dispatch({
-        type: FETCH_USER,
-        payload: res
-      })
-    );
-  };
+// Refactor using arrow and async/await
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+
+  dispatch({ type: FETCH_USER, payload: res });
 };
+
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios.get("/api/current_user").then(res =>
+//       dispatch({
+//         type: FETCH_USER,
+//         payload: res
+//       })
+//     );
+//   };
+// };
