d5d89bde94301a20f5bea46fc4180897f9402766
rayGreycloud <rayGreycloud@gmail.com>
2017-07-31 08:47:19 -0700

    Render status message in header based on auth status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,18 +2,32 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Header extends Component {
-  render() {
-    // Test prop connection
-    console.log(this.props);
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
 
+  render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="brand-logo left">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
