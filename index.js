1a35450df91a23048457177175c9ad4a72773857
rayGreycloud <rayGreycloud@gmail.com>
2017-08-02 06:37:07 -0700

    Update survey model; scaffold survey routes

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(passport.session());
 // Skip variable assignment - DRY
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express serve production assets
