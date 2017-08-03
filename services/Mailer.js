bec361005b124097135806a849faa98583c36544
rayGreycloud <rayGreycloud@gmail.com>
2017-08-03 08:47:39 -0700

    Enable sendgrid click tracking

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,10 +6,37 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    // Set up email properties
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+    // built-in method to load email content
+    this.addContent(this.body);
+    // enable click tracking
+    this.addClickTracking();
+    // add formatted addresses
+    this.addRecipients();
+  }
+  // Format email addresses
+  formatAddresses(recipients) {
+    // Note - must wrap destructured {variable} with ()
+    // Pull off each recipient
+    return recipients.map(({ email }) => {
+      // Format address and return
+      return new helper.Email(email);
+    });
+  }
+  // Click tracking
+  addClickTracking() {
+    // create helper instance
+    const trackingSettings = new helper.TrackingSettings();
+    // turn tracking on
+    const clickTracking = new helper.ClickTracking(true, true);
+    // Set tracking on
+    trackingSettings.setClickTracking(clickTracking);
+    // Add setting
+    this.addTrackingSettings(trackingSettings);
   }
 }
 
