40f048560e9df5b95c9e93f47a9d5cb683108976
rayGreycloud <rayGreycloud@gmail.com>
2017-08-03 11:36:40 -0700

    Remove test code

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,6 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
-// For testing
-import axios from 'axios';
-window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
