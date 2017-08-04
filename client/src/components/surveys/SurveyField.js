da7060c897a5cc1bf0fcadf16b9dcdc81f83c679
rayGreycloud <rayGreycloud@gmail.com>
2017-08-04 08:49:58 -0700

    Add survey field component and renderFields method

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render single label and text input
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
