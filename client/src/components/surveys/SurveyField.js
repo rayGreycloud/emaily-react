2a4a9bf598a785444ddabe543cb9d33541d1c1b4
rayGreycloud <rayGreycloud@gmail.com>
2017-08-04 08:57:21 -0700

    Wire up custom survey field to survey form component

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 // SurveyField contains logic to render single label and text input
 import React from 'react';
 
-export default () => {
+// Pull off input from props object
+export default ({ input }) => {
+  // Use spread operator to pass everything to input tag
   return (
     <div>
-      <input />
+      <input {...input} />
     </div>
   );
 };
