000417ad587adea5326dae34d424a1ba7675a009
rayGreycloud <rayGreycloud@gmail.com>
2017-08-04 09:04:13 -0700

    Add label to surveyField and add form fields to survey form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,14 @@
 // SurveyField contains logic to render single label and text input
 import React from 'react';
 
-// Pull off input from props object
-export default ({ input }) => {
+// Pull off input and label title from props object
+export default ({ input, label }) => {
   // Use spread operator to pass everything to input tag
   return (
     <div>
+      <label>
+        {label}
+      </label>
       <input {...input} />
     </div>
   );
