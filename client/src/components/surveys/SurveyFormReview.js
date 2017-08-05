60d24c8415c06c99ace4d5676b667a60b0512993
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 08:44:33 -0700

    Add survey form review component

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SurveyFormReview extends Component {
+  render() {
+    return (
+      <h3>Survey Form Review</h3>
+      <h5>Please confirm your entries</h5>
+    );
+  }
+}
+
+export default SurveyFormReview;
