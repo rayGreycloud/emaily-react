172354dd8eb45d7fb723cf99fa287af52d71d6cc
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 08:40:45 -0700

    Add state to component

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,16 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  // Classic method to set state
+  constructor(props) {
+    super(props);
+
+    this.state = { formReview: false };
+  }
+
   render() {
     return (
       <div>
