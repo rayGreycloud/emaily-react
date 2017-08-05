60af8494bc00250502a2cb7a48721bafac9c9ed8
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 08:44:16 -0700

    Refactor state declaration using create-react-app babel plugin

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -4,12 +4,15 @@ import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-  // Classic method to set state
-  constructor(props) {
-    super(props);
+  // // Classic method to set state
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { formReview: false };
+  // }
 
-    this.state = { formReview: false };
-  }
+  // Setting state with babel plugin
+  state = { formReview: false };
 
   render() {
     return (
