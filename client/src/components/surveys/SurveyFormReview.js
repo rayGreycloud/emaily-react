fe86284d2d52000f129474daa6407a12f36bd76f
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 09:41:02 -0700

    Persist and access survey form values

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -5,7 +5,7 @@ const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button className="yellow darken-2 btn-flat" onClick={onCancel}>
         Back
       </button>
     </div>
