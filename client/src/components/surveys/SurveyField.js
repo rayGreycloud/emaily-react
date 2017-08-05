1865e2c2197e2bbfffeef93856d25de0cd3beec1
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 07:13:44 -0700

    Style error message

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -10,8 +10,10 @@ export default ({ input, label, meta: { error, touched } }) => {
       <label>
         {label}
       </label>
-      <input {...input} />
-      {touched && error}
+      <input {...input} style={{ marginBottom: '5px' }} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
