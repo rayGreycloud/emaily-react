a0addac7df9294663ad8e14b98bbd258f9800e0f
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 08:58:17 -0700

    Fix jsx error

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class SurveyFormReview extends Component {
   render() {
     return (
-      <h3>Survey Form Review</h3>
-      <h5>Please confirm your entries</h5>
+      <div>
+        <h3>Survey Form Review</h3>
+        <h5>Please confirm your entries</h5>
+      </div>
     );
   }
 }
