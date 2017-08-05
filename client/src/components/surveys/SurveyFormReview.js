01223085a107c9ac58ef975aca6b4beb700e081c
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 10:43:13 -0700

    Render form input on survey form review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,19 +1,27 @@
 // SurveyFormReview shows users form inputs to review and confirm
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ label, name }) => {
+    return (
+      <div key={name}>
+        <label>
+          {label}
+        </label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>
-            {formValues.title}
-          </div>
-        </div>
-      </div>
+      {reviewFields}
       <button className="yellow darken-2 btn-flat" onClick={onCancel}>
         Back
       </button>
