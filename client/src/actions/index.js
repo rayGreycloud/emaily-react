9ae8c9fcd6af7af0077fc629818cde1914db82fe
rayGreycloud <rayGreycloud@gmail.com>
2017-08-05 12:18:57 -0700

    Add submit survey action and connect to button

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-// Refactor using arrow and async/await
+// Fetch user
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   // Only need data prop
@@ -13,6 +13,11 @@ export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+// Handle survey form submit
+export const submitSurvey = values => {
+  return { type: 'submit_survey' };
 };
 
 // export const fetchUser = () => {
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ label, name }) => {
     return (
       <div key={name}>
@@ -25,7 +26,10 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       <button className="teal darken-1 white-text btn-flat" onClick={onCancel}>
         Back
       </button>
-      <button className="blue darken-3 right white-text btn-flat">
+      <button
+        className="blue darken-3 right white-text btn-flat"
+        onClick={() => submitSurvey(formValues)}
+      >
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -39,4 +43,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
